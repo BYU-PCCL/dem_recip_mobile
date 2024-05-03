d7a8eefe987159764edcce943ff6577c323cd406
Ty Klabacka <klabacka.ty@gmail.com>
2024-05-03 14:04:12 -0600

    fixed tab navigator

--- app/lib/view/tab_navigation.dart
+++ app/lib/view/tab_navigation.dart
@@ -1,29 +1,53 @@
+import 'package:flutter/material.dart';
 import 'package:dem_recip_mobile/view/home_view.dart';
 import 'package:dem_recip_mobile/view/profile_view.dart';
-import 'package:flutter/material.dart';
 
+class TabNav extends StatefulWidget {
+  const TabNav({Key? key}) : super(key: key);
 
-class TabNav extends StatelessWidget {
-  const TabNav({super.key});
+  @override
+  _TabNavState createState() => _TabNavState();
+}
+
+class _TabNavState extends State<TabNav> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _tabs = [
+    HomeView(),
+    ProfileView(),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: const TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.directions_car)),
-              Tab(icon: Icon(Icons.directions_bike)),
-            ],
-          ),
-          title: const Text('Tabs Demo'),
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _tabs,
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          splashColor: Colors.transparent, // Set splash color to transparent
+          highlightColor: Colors.transparent, // Set highlight color to transparent
+          splashFactory: NoSplash.splashFactory, // Use NoSplash factory to disable splash
         ),
-        body: const TabBarView(
-          children: <Widget>[
-            HomeView(),
-            ProfileView()
+        child: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onTabTapped,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.directions_car),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.directions_bike),
+              label: 'Profile',
+            ),
           ],
         ),
       ),
